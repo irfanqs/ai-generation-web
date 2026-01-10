b58342113a13321f1e0f134789e9c3ccc758dd1a
irfanqs <irfanqobus.01@gmail.com>
2026-01-10 22:37:55 +0700

    feat: add redis upstash config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -18,6 +18,8 @@ import { AdminModule } from './admin/admin.module';
       redis: {
         host: process.env.REDIS_HOST || 'localhost',
         port: parseInt(process.env.REDIS_PORT) || 6379,
+        password: process.env.REDIS_PASSWORD || undefined,
+        tls: process.env.REDIS_TLS === 'true' ? {} : undefined,
       },
     }),
     PrismaModule,
